6c35853dcd911783b08e17680232d05bf336cfd2
zhany-bek <daveelfie@gmail.com>
2022-12-13 14:27:41 +0600

    More practice

--- javascript.js
+++ javascript.js
@@ -1,3 +1,1 @@
-const OUR_PLANET_NAME = "Earth";
-let currentVisitor;
-// Did half of the first item on Numbers (https://www.theodinproject.com/lessons/foundations-fundamentals-part-1)
+// Stopped on comparisons
